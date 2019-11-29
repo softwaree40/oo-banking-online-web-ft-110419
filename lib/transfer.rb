15d6166f766c4916c77d92a46809b3fd29a6e2a9
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 03:56:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def has_enough_funds?(person)
+    person.balance >= self.amount
+  end
+
+  def execute_transaction
+    if self.status != "complete"
+      if self.valid? && self.has_enough_funds?(self.sender) && self.has_enough_funds?(self.receiver)
+        self.sender.withdraw(self.amount)
+        self.receiver.deposit(self.amount)
+        self.status = "complete"
+      else
+
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.receiver.withdraw(self.amount)
+      self.sender.deposit(self.amount)
+      self.status = "reversed"
+    end
+  end
 end
