0b61ddf061c97c3f009337e1ab9fa6986bbfdff1
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 03:56:34 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+attr_reader :name
+  attr_accessor :balance, :status
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    self.balance += money
+  end
+
+  def withdraw(money)
+    self.balance -= money if self.valid?
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
